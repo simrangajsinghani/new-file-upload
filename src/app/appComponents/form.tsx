0dea0d63e66511253fb948969a0f4703f665ed0c
simrangajsinghani <simrangajsinghani259@gmail.com>
2024-04-04 22:21:19 +0530

    Modal added

--- src/app/appComponents/form.tsx
+++ src/app/appComponents/form.tsx
@@ -2,50 +2,92 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export default function UploadForm() {
   const [file, setFile] = useState<File | null>(null);
+  const [showModal, setShowModal] = useState<boolean>(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append("file", file as Blob);
-    const response = await fetch("/api/file", {
-      method: "POST",
-      body: formData,
-    });
+    console.log(file?.size, "size");
+    if (file && file?.size > 1048576) {
+      console.log("true");
+      setShowModal(true);
+    } else {
+      console.log(showModal, "false");
+      const formData = new FormData();
+      formData.append("file", file as Blob);
+      // const response = await fetch("/api/file", {
+      //   method: "POST",
+      //   body: formData,
+      // });
 
-    // try {
-    //   const response = await fetch("/api/file", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json", // Specify content type
-    //       // Add any other headers as needed
-    //     },
-    //     body: JSON.stringify(formData), // Convert data to JSON string
-    //   });
+      try {
+        const response = await fetch("/api/file", {
+          method: "POST",
+          // headers: {
+          //   "Content-Type": "application/json", // Specify content type
+          //   // Add any other headers as needed
+          // },
+          body: formData,
+        });
 
-    //   if (!response.ok) {
-    //     throw new Error("Network response was not ok");
-    //   }
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
 
-    //   const data = await response.json(); // Parse response as JSON
+        const data = await response.json(); // Parse response as JSON
 
-    //   // Handle successful response
-    //   console.log("Success:", data);
-    // } catch (error) {
-    //   // Handle error
-    //   console.error("Error:", error);
-    // }
+        // Handle successful response
+        console.log("Success:", data);
+      } catch (error) {
+        // Handle error
+        console.error("Error:", error);
+      }
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Input
-        type="file"
-        onChange={(e) => setFile(e.target.files?.item(0) || null)}
-      />
-      <Button type="submit">Upload</Button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="file"
+          onChange={(e) => setFile(e.target.files?.item(0) || null)}
+        />
+        <Button type="submit">Upload</Button>
+      </form>
+
+      {
+        <AlertDialog open={showModal}>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>
+                <p className="text-gray-200">
+                  File size exceeds 5MB. Please choose a smaller file.
+                </p>
+              </AlertDialogTitle>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogAction onClick={() => setShowModal(!showModal)}>
+                <Button variant="default" size="sm">
+                  OK
+                </Button>
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      }
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,12 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
-  darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -19,11 +18,11 @@ const config = {
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        // border: "hsl(var(--border))",
+        // input: "hsl(var(--input))",
+        // ring: "hsl(var(--ring))",
+        // background: "hsl(var(--background))",
+        // foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
@@ -32,49 +31,35 @@ const config = {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+        // destructive: {
+        //   DEFAULT: "hsl(var(--destructive))",
+        //   foreground: "hsl(var(--destructive-foreground))",
+        // },
+        // muted: {
+        //   DEFAULT: "hsl(var(--muted))",
+        //   foreground: "hsl(var(--muted-foreground))",
+        // },
+        // accent: {
+        //   DEFAULT: "hsl(var(--accent))",
+        //   foreground: "hsl(var(--accent-foreground))",
+        // },
+        // popover: {
+        //   DEFAULT: "hsl(var(--popover))",
+        //   foreground: "hsl(var(--popover-foreground))",
+        // },
+        // card: {
+        //   DEFAULT: "hsl(var(--card))",
+        //   foreground: "hsl(var(--card-foreground))",
+        // },
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
